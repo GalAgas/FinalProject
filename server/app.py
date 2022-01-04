129080038adf3e8d7d3c2220089277170cf520fe
tomerkel <73695609+tomerkel@users.noreply.github.com>
2022-01-04 19:37:54 +0200

    add some comments

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ DEBUG = True
 
 class EndpointAction(object):
     """
-    Endpoint class - contains the backend.
+    Endpoint class.
     when it called performers the action
     """
 
@@ -55,6 +55,12 @@ class WebService(object):
             handler), methods=['GET', 'POST'])
 
     def response(self, message, status):
+        """
+
+        :param message: message to send, str
+        :param status: HTTP response code, int
+        :return: response to client
+        """
         response = self.app.response_class(
             response=json.dumps(message),
             status=status,
@@ -63,6 +69,10 @@ class WebService(object):
         return response
 
     def generate_recommendation(self):
+        """
+        the function validates that input is valid and then generates a treatment recommendation table based on input.
+        :return: recommendation table as json
+        """
         gene_correlation_csv = request.files['gene_correlation_csv']
         gene_correlation_txt = request.files['gene_correlation_txt']
         patient_id = request.form['id']
@@ -79,7 +89,6 @@ class WebService(object):
             if message != '':
                 return self.response(message, 400)
             mic = self.mic_predictor.predict(csv_file, txt_file)
-            mic = self.mic_predictor.predict(csv_file, None)
             initial_ranking = self.treatment_ranking.rank(mic)
             final_ranking = self.context_aware.rank(initial_ranking)
             return jsonify(final_ranking)
@@ -92,6 +101,11 @@ class WebService(object):
         return True
 
     def check_valid_txt(self, gene_correlation_txt):
+        """
+        check if the given txt file is in the requested format.
+        :param gene_correlation_txt: the system input - file of gene correlation, txt file
+        :return: error message or empty message according to validation
+        """
         message = ''
         float_regex = '[0-9]+\.[0-9]+'
         for i, row in enumerate(gene_correlation_txt):
@@ -99,16 +113,21 @@ class WebService(object):
             row_tokens = row.split(" ")
             if not row_tokens[0].startswith(">contig"):
                 message = f'line {i+1} , column 1 starts with invalid value'
-                # maybe to add break after we found the first problem ???
+                break
             if not row_tokens[1].startswith("len=") or not row_tokens[1][4:].isdecimal():
                 message = f'line {i+1}, column 2 has invalid value'
-                # maybe to add break after we found the first problem ???
+                break
             if not row_tokens[2].startswith("cov=") or not re.search(float_regex,row_tokens[2][4:]):
                 message = f'line {i+1}, column 3 has invalid value'
-                # maybe to add break after we found the first problem ???
+                break
         return message
 
     def check_valid_csv(self, gene_correlation_csv):
+        """
+        check if the given csv file is in the requested format.
+        :param gene_correlation_csv: the system input - file of gene correlation, csv file
+        :return: error message or empty message according to validation
+        """
         message = ''
         required_columns = {"file": str, "gene": str, "contig": str, "start": int,
                             "end": int, "depth": float, "seqid": float, "seqcov": float,
@@ -117,43 +136,44 @@ class WebService(object):
         # rename columns name to lower case
         gene_correlation_csv.columns = gene_correlation_csv.columns.str.lower()
         # check if all the required columns exists
-        if len(required_columns.keys()) != len(gene_correlation_csv.columns):
-            message = f'expected {len(required_columns.keys())} columns but got {len(gene_correlation_csv.columns)}'
-        for col_name in required_columns.keys(): #checked ok
+        for col_name in required_columns.keys():
             if col_name not in gene_correlation_csv.columns:
                 message = f'column "{col_name}" is missing'
+                return message
+        if len(required_columns.keys()) != len(gene_correlation_csv.columns):
+            message = f'expected {len(required_columns.keys())} columns but got {len(gene_correlation_csv.columns)}'
         # check if the 'file' column have 1 value
-        if len(gene_correlation_csv["file"].unique()) != 1: 
+        elif len(gene_correlation_csv["file"].unique()) != 1:
             message = f'expected 1 unique value in column "file" but got {len(gene_correlation_csv["file"].unique())}'
         # check there is no missing values in 'gene' column
-        if gene_correlation_csv["gene"].isnull().any(): 
+        elif gene_correlation_csv["gene"].isnull().any():
             message = "column 'gene' can't have missing values."
         # check contig column values are in right format.
-        for i, val in enumerate(gene_correlation_csv["contig"]): 
-            if not val.lower().startswith("contig") or not val[6:].isdigit():
-                message = f'Invalid value in column "contig" row {i+2}'
-        # check if all values in int/float columns are int/float and in the neccesary range.
-        for col_name, col_type in required_columns.items():
-            if col_type == int:
-                if not all(isinstance(val, int) for val in gene_correlation_csv[col_name]): 
-                    message = f'column {col_name} have invalid value - Not all values are decimal numbers'
-            elif col_type == float:
-                if not all(isinstance(val, float) or isinstance(val, int) for val in gene_correlation_csv[col_name]):
-                    message = f'column {col_name} have invalid value - Not all values are real numbers'
-            if col_name == "seqid" or col_name == "seqcov":
-                if not all(0 <= val <= 100 for val in gene_correlation_csv[col_name]):
-                    message = f'column {col_name} have invalid value'
-        print(message)
+        else:
+            for i, val in enumerate(gene_correlation_csv["contig"]):
+                if not val.lower().startswith("contig") or not val[6:].isdigit():
+                    message = f'Invalid value in column "contig" row {i+2}'
+                    return message
+            # check if all values in int/float columns are int/float and in the neccesary range.
+            for col_name, col_type in required_columns.items():
+                if col_type == int:
+                    if not all(isinstance(val, int) for val in gene_correlation_csv[col_name]):
+                        message = f'column {col_name} have invalid value - Not all values are decimal numbers'
+                elif col_type == float:
+                    if not all(isinstance(val, float) or isinstance(val, int) for val in gene_correlation_csv[col_name]):
+                        message = f'column {col_name} have invalid value - Not all values are real numbers'
+                if col_name == "seqid" or col_name == "seqcov":
+                    if not all(0 <= val <= 100 for val in gene_correlation_csv[col_name]):
+                        message = f'column {col_name} have invalid value'
         return message
 
     def read_txt_file(self, file):
-        # file = open(path, "r")
         return file.readlines()
 
-    def read_csv_file(self, path):
-        return pd.read_csv(path, index_col=0)
+    def read_csv_file(self, file):
+        return pd.read_csv(file, index_col=0)
 
-
+# open the server class. add endpoint and start running the server.
 web_service_app = WebService('webservice')
 web_service_app.add_endpoint(endpoint='/generate', endpoint_name='generate recommendation',
                              handler=web_service_app.generate_recommendation)
