275e47715f83c1dd2fac9cce3f2d72c2819483b8
GalAgas <galagasi5@gmail.com>
2022-01-04 18:16:54 +0200

    complete input validation

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from MICPredictor import MICPredictor
 from treatmentRanking import treatmentRanking
 from contextAware import contextAware
+import re
 
 
 # configuration
@@ -69,12 +70,15 @@ class WebService(object):
             if not self.check_valid_id(patient_id):
                 return self.response("Invalid Id", 409)
             csv_file = self.read_csv_file(gene_correlation_csv)
-            # txt_file = self.read_txt_file(gene_correlation_txt)
+            txt_file = self.read_txt_file(gene_correlation_txt)
+            # print(txt_file)
             message = self.check_valid_csv(csv_file)
             if message != '':
                 return self.response(message, 400)
-            # self.check_valid_txt(txt_file)
-            # mic = self.mic_predictor.predict(csv_file, txt_file)
+            message = self.check_valid_txt(txt_file)
+            if message != '':
+                return self.response(message, 400)
+            mic = self.mic_predictor.predict(csv_file, txt_file)
             mic = self.mic_predictor.predict(csv_file, None)
             initial_ranking = self.treatment_ranking.rank(mic)
             final_ranking = self.context_aware.rank(initial_ranking)
@@ -88,18 +92,23 @@ class WebService(object):
         return True
 
     def check_valid_txt(self, gene_correlation_txt):
+        message = ''
+        float_regex = '[0-9]+\.[0-9]+'
         for i, row in enumerate(gene_correlation_txt):
+            row = row[:-1].decode("utf-8") # remove '\n' and convert from bytes to string
             row_tokens = row.split(" ")
             if not row_tokens[0].startswith(">contig"):
-                return self.response(f'line {i} , column 1 starts with invalid value', 400)
-            if not row_tokens[1].startswith("len=") or not isinstance(row_tokens[1][4:], int):
-                return self.response(f'line {i}, column 2 has invalid value', 400)
-            if not row_tokens[2].startswith("cov=") or not isinstance(row_tokens[2][4:], float):
-                return self.response(f'line {i}, column 3 has invalid value', 400)
+                message = f'line {i+1} , column 1 starts with invalid value'
+                # maybe to add break after we found the first problem ???
+            if not row_tokens[1].startswith("len=") or not row_tokens[1][4:].isdecimal():
+                message = f'line {i+1}, column 2 has invalid value'
+                # maybe to add break after we found the first problem ???
+            if not row_tokens[2].startswith("cov=") or not re.search(float_regex,row_tokens[2][4:]):
+                message = f'line {i+1}, column 3 has invalid value'
+                # maybe to add break after we found the first problem ???
+        return message
 
-    ## NEED TO CHECK ALL CASES ##
     def check_valid_csv(self, gene_correlation_csv):
-        # check if the file is csv/xslx - front!
         message = ''
         required_columns = {"file": str, "gene": str, "contig": str, "start": int,
                             "end": int, "depth": float, "seqid": float, "seqcov": float,
@@ -108,43 +117,41 @@ class WebService(object):
         # rename columns name to lower case
         gene_correlation_csv.columns = gene_correlation_csv.columns.str.lower()
         # check if all the required columns exists
-        # print(gene_correlation_csv.columns)
-        # print(required_columns.keys())
-        if len(required_columns.keys())+1 != len(gene_correlation_csv.columns):
+        if len(required_columns.keys()) != len(gene_correlation_csv.columns):
             message = f'expected {len(required_columns.keys())} columns but got {len(gene_correlation_csv.columns)}'
-        for col_name in required_columns.keys():
+        for col_name in required_columns.keys(): #checked ok
             if col_name not in gene_correlation_csv.columns:
-                message = f'column "{col_name}" missing'
+                message = f'column "{col_name}" is missing'
         # check if the 'file' column have 1 value
-        if len(gene_correlation_csv["file"].unique()) != 1:
+        if len(gene_correlation_csv["file"].unique()) != 1: 
             message = f'expected 1 unique value in column "file" but got {len(gene_correlation_csv["file"].unique())}'
         # check there is no missing values in 'gene' column
-        if gene_correlation_csv["gene"].isnull().any():
+        if gene_correlation_csv["gene"].isnull().any(): 
             message = "column 'gene' can't have missing values."
         # check contig column values are in right format.
-        for i, val in enumerate(gene_correlation_csv["contig"]):
+        for i, val in enumerate(gene_correlation_csv["contig"]): 
             if not val.lower().startswith("contig") or not val[6:].isdigit():
-                message = f'Invalid value in column "contig" row {i}'
-        # check if all values in int/float columns are int/float.
-        # for i, col_name, col_type in enumerate(required_columns.items()):
-        #     if col_type == int:
-        #         if not all(isinstance(val, int) for val in gene_correlation_csv[col_name]):
-        #             message = f'column {col_name} have invalid value in row {i}'
-        #     elif col_type == float:
-        #         if not all(isinstance(val, float) or isinstance(val, int) for val in gene_correlation_csv[col_name]):
-        #             message = f'column {col_name} have invalid value in row {i}'
-        #     if col_name == "seqid" or col_name == "seqcov":
-        #         if not all(0 < val < 100 for val in gene_correlation_csv[col_name]):
-        #             message = f'column {col_name} have invalid value in row {i}'
-        # print(message)
+                message = f'Invalid value in column "contig" row {i+2}'
+        # check if all values in int/float columns are int/float and in the neccesary range.
+        for col_name, col_type in required_columns.items():
+            if col_type == int:
+                if not all(isinstance(val, int) for val in gene_correlation_csv[col_name]): 
+                    message = f'column {col_name} have invalid value - Not all values are decimal numbers'
+            elif col_type == float:
+                if not all(isinstance(val, float) or isinstance(val, int) for val in gene_correlation_csv[col_name]):
+                    message = f'column {col_name} have invalid value - Not all values are real numbers'
+            if col_name == "seqid" or col_name == "seqcov":
+                if not all(0 <= val <= 100 for val in gene_correlation_csv[col_name]):
+                    message = f'column {col_name} have invalid value'
+        print(message)
         return message
 
-    def read_txt_file(self, path):
-        file = open(path, "r")
+    def read_txt_file(self, file):
+        # file = open(path, "r")
         return file.readlines()
 
     def read_csv_file(self, path):
-        return pd.read_csv(path)
+        return pd.read_csv(path, index_col=0)
 
 
 web_service_app = WebService('webservice')
