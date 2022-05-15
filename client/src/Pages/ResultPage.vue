a86516723b11f75c2112c20ed479fe3ff0082e31
tomerkel <73695609+tomerkel@users.noreply.github.com>
2022-05-15 12:43:00 +0300

    expend table and add pregnancy button

--- client/src/Pages/ResultPage.vue
+++ client/src/Pages/ResultPage.vue
@@ -5,25 +5,29 @@
     <br/>
     <br/>
     <div class="table_div">
-        <b-table striped hover :items="res" :fields="fields" id="results"
+        <b-table striped hover :items="res" :fields="visibleFields" id="results"
          :dark="true" :filter="confidenceFilter" :filter-function="filterTable">
           <template #cell(index)="data">
             {{ data.index + 1 }}
           </template>
-          <!-- <template #cell(Details)="data">
-            <a :href="`https://www.drugs.com/search.php?searchterm=${data.item.Drug_Name}`">More Details</a>
-          </template> -->
           <template #cell(Drug_Name)="data">
             <a :href="`https://www.drugs.com/search.php?searchterm=${data.item.Drug_Name}`" target="_blank">{{ data.item.Drug_Name }}</a>
           </template>
         </b-table>
     </div>
-    <br/>
     <div>
       <h4><b>* Click On Drug Name For More Information &nbsp;
         <b-icon icon="info-circle-fill" scale="2">
       </b-icon></b></h4>
     </div>
+    <b-button
+      id="expandBtn"
+      size="lg"
+      pill variant="info"
+      class="ml-5w-75"
+      v-on:click="changeFields">
+      {{ expend_button_text }}
+    </b-button>
     <br/>
     <br/>
 
@@ -52,11 +56,22 @@ export default {
     return {
       res: null,
       confidenceFilter: '',
-      fields: ['index', 'Drug_Name', { key: 'MIC' }, { key: 'MIC_Confidence' }, { key: 'Comments' }],
+      fields: [{ key: 'index', visable: true },
+        { key: 'Drug_Name', visable: true },
+        { key: 'MIC', visable: true },
+        { key: 'MIC_Confidence', visable: true },
+        { key: 'Coverage', visable: false },
+        { key: 'Comments', visable: true }],
+      expend_button_text: 'Expend View',
     };
   },
   created() {
     this.res = this.$route.params.res;
+  },
+  computed: {
+    visibleFields() {
+      return this.fields.filter((filed) => filed.visable);
+    },
   },
   methods: {
     filterTable(row, filter) {
@@ -68,11 +83,18 @@ export default {
       }
       return true;
     },
-    // linkCreate(drugName) {
-    //   console.log(drugName);
-    //   const href = 'https://www.drugs.com/search.php?searchterm=';
-    //   return href + drugName;
-    // },
+    changeFields() {
+      const keysToChange = { Coverage: 4 };
+      Object.keys(keysToChange).forEach((field) => {
+        if (this.fields[keysToChange[field]].visable) {
+          this.fields[keysToChange[field]].visable = false;
+          this.expend_button_text = 'Expend View';
+        } else {
+          this.fields[keysToChange[field]].visable = true;
+          this.expend_button_text = 'Hidden View';
+        }
+      });
+    },
   },
 };
 </script>
